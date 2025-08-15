dd4440adef2249f7a850070f1526bb14d9eeb84f
Sam <s.bartlett@slcyber.io>
2025-08-15 10:07:23 +0100

    repo reset (again)

--- test.py
+++
@@ -1,7 +0,0 @@
-import json
-
-test_file = json.load(open("arc-prize-2025/arc-agi_training_challenges.json"))
-
-print(json.dumps(test_file["ff805c23"]["train"], indent=2))
-
-# print(json.dumps(test_file["ff805c23"], indent=2))
